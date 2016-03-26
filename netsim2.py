d1fab8943ab7cb6d9076ef7df7ecacae5f210275
vkuruturi <kurutu@cooper.edu>
2016-03-26 12:30:56 -0400

    deleted the code from netsim2 that was moved to packet
    imported packet in netsim2

--- netsim2.py
+++ netsim2.py
@@ -1,34 +1,7 @@
 #The following classes define all the contents of a packet
 
-class IPHeader:
-	def __init__(self,timeToLive,sourceAddress,destinationAddress):
-		self.timeToLive = timeToLive
-		self.sourceAddress = sourceAddress
-		self.destinationAddress = destinationAddress
-	#The avove are important to the simulation
-	#The following were left out because they are likely irrelevant
-	#Version, header length, TOS, total length, identification, flags,
-	#header checksum, fragment offset, protocol, options
-	
-class TCPHeader:
-	def __init__(srcPort,dstPort,seq,ack,cwnd):
-		self.sourcePort = srcPort
-		self.destinationPort = dstPort
-		self.sequenceNumber = seq
-		self.acknowledgeNumber = ack
-		self.windowSize = cwnd
-	#The above are important to the simulation
-	#The following were left out because they are likely irrelevent
-	#Data offset, Reserved, flags, urgent pointer, checksum, options,
-	#padding, Data
-	
-class Packet:
-	def __init__(TCPHeader,IPHeader,immediateSender):
-		self.tcpHeader = TCPHeader
-		self.ipHeader = IPHeader
-		self.immediateSender = immediateSender
+import packet
 
-#This line marks the end of the packet class definitions		
 
 #This line marks the start of TCP algorithm definitions
 
